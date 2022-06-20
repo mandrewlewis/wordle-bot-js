dd36e9d578baebd724a396cc939b65cc227582cb
Andrew Lewis <andrewdavidlewis@gmail.com>
2022-06-19 23:16:27 -0700

    Create button functionality to display new row

--- wordle_page.js
+++ wordle_page.js
@@ -1,16 +1,58 @@
 const toggleList = ['none','wrong','misplaced','correct'];
-const sendButton = document.querySelector('.send-feedback');
 const letterButtons = document.querySelectorAll('.letter');
+const btnOne = document.querySelector('#btnOne');
+const btnTwo = document.querySelector('#btnTwo');
+const btnThree = document.querySelector('#btnThree');
+const btnFour = document.querySelector('#btnFour');
+const btnFive = document.querySelector('#btnFive');
+const btnSix = document.querySelector('#btnSix');
+let rowLock = [];
 
-sendButton.addEventListener('click', () => {
-    // Send feedback and get new word to guess
+// Button listner
+btnOne.addEventListener('click', e => {
+    sendFeedback(1, 'two');
+    btnOne.disabled = true;
+    btnOne.style.visibility = 'hidden';
+});
+btnTwo.addEventListener('click', e => {
+    sendFeedback(2, 'three');
+    btnTwo.disabled = true;
+    btnTwo.style.visibility = 'hidden';
+});
+btnThree.addEventListener('click', e => {
+    sendFeedback(3, 'four');
+    btnThree.disabled = true;
+    btnThree.style.visibility = 'hidden';
+});
+btnFour.addEventListener('click', e => {
+    sendFeedback(4, 'five');
+    btnFour.disabled = true;
+    btnFour.style.visibility = 'hidden';
+});
+btnFive.addEventListener('click', e => {
+    sendFeedback(5, 'six');
+    btnFive.disabled = true;
+    btnFive.style.visibility = 'hidden';
+});
+btnSix.addEventListener('click', e => {
+    sendFeedback(6, 'seven');
+    btnSix.disabled = true;
+    btnSix.style.visibility = 'hidden';
 });
 
+
+// Onclick for letters
 document.addEventListener('click', e => {
     if (!e.target.classList.contains('letter')) return;
 
     let div = e.target;
     let index = 0;
+
+    //Break if row locked from chaning colors
+    ///////////////////
+    // add class 'one', 'two', etc to 'letter-group'
+    // check this event listner for the parent
+    // ???
 
     toggleList.forEach(x => {
         if (div.classList.contains(x)) index = toggleList.indexOf(x);
@@ -19,5 +61,15 @@ document.addEventListener('click', e => {
     div.classList.toggle(toggleList[index]);
     index === 3 ? index = 1 : index++;
     div.classList.toggle(toggleList[index]);
-})
+});
 
+
+function sendFeedback(round, nextRow) {
+    // Send feedback and get new word to guess
+    // this line here
+
+    if (round === 6) { alert('you lost!'); return;}
+    // Display new row
+    let newRow = document.querySelector(`.round.${nextRow}`);
+    newRow.style.visibility = 'visible';
+}
